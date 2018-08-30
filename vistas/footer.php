fdecc20206108787c7d4131c00dac1d7dd562ea8
Martin <magadiflo@gmail.com>
2018-08-30 12:52:39 -0500

    Distribución de plantilla en el proyecto, division de header.php y footer.php para común a todas las vistas a incorporar. Funcionamiento correcto de la plantilla en el proyecto

---
+++ vistas/footer.php
@@ -0,0 +1,18 @@
+
+    <footer class="main-footer">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+      </div>
+      <strong>Copyright &copy; 2018 <a href="www.incanatoit.com">IncanatoIT</a>.</strong> All rights reserved.
+    </footer>
+    <!-- Fin de wrapper -->
+  </div>
+  <!-- jQuery 2.1.4 -->
+  <script src="../public/js/jquery-3.1.1.min.js"></script>
+  <!-- Bootstrap 3.3.5 -->
+  <script src="../public/js/bootstrap.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../public/js/app.min.js"></script>
+</body>
+
+</html>
